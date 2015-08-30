5c2e68d1ee2cd546d63fdb44e4ec1c7003c5da87
Mateusz Nowaczyk <szajbus20@gmail.com>
2015-08-30 11:57:49 +0200

    Adding GruntJS

---
+++ gruntfile.js
@@ -0,0 +1,47 @@
+
+'use strict';
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '// <%=pkg.author %> 2015\n// <%= pkg.name %> - <%= pkg.description %>\n' +
+						'//----------------------------------------------------------\n\n'
+			},
+			my_target: {
+				files: {
+					'js/output.min.js': ['js/respond.js', 'js/modernizr-2.8.3.min.js',
+					'js/bootstrap.min.js', 'js/plugins.js', 'js/main.js']
+				}
+			}
+		},
+		cssmin: {
+			combine: {
+				files: {
+					'css/output.min.css': ['css/bootstrap.min.css', 'css/normalize.css',
+					'css/main.css', 'css/font-awesome.min.css']
+				}
+			}
+		},
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie 8', 'ie 9']
+			},
+			main: {
+				expand: true,
+				flatten: true,
+				src: 'scss/main.scss',
+				dest: 'scss/'
+			}
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+
+	grunt.registerTask('default', ['cssmin', 'uglify', 'autoprefixer']);
+
+};
