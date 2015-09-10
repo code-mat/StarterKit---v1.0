aef933d8c9734b846856af88ee312306560a1c3f
Mateusz Nowaczyk <szajbus20@gmail.com>
2015-09-10 16:18:19 +0200

    Adding GruntJS functions, update index and IE conditionals

--- gruntfile.js
+++ gruntfile.js
@@ -5,6 +5,14 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			scripts: {
+				files: ['js/respond.js', 'js/modernizr-2.8.3.min.js', 'js/main.js', 'js/plugins.js',
+				'js/jquery.corner.js', 'js/jquery.placeholder.min.js', 'css/normalize.css',
+				'css/font-awesome.min.css', 'css/main.css', 'css/ie-main.css'],
+				tasks: ['uglify', 'cssmin', 'autoprefixer']
+			}
+		},
 		uglify: {
 			options: {
 				banner: '// <%=pkg.author %> 2015\n// <%= pkg.name %> - <%= pkg.description %>\n' +
@@ -12,15 +20,16 @@ module.exports = function(grunt) {
 			},
 			my_target: {
 				files: {
-					'js/output.min.js': ['js/bootstrap.min.js', 'js/plugins.js', 'js/main.js']
+					'js/output.min.js': ['js/plugins.js', 'js/main.js'],
+					'js/ie-scripts.min.js': ['js/jquery.corner.js', 'js/jquery.placeholder.min.js', 'js/main.js']
 				}
 			}
 		},
 		cssmin: {
 			combine: {
 				files: {
-					'css/output.min.css': ['css/bootstrap.min.css', 'css/normalize.css',
-					'css/main.css', 'css/font-awesome.min.css']
+					'css/output.min.css': ['css/normalize.css', 'css/font-awesome.min.css', 'css/main.css'],
+					'css/ie-main.min.css': ['css/ie-main.css']
 				}
 			}
 		},
@@ -37,6 +46,7 @@ module.exports = function(grunt) {
 		},
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-autoprefixer');
